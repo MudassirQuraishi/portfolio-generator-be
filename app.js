93632acc0dedc035924e5de8a029028887958dcc
MudassirQuraishi <mudassir.quraishi14@outlook.com>
2024-03-16 18:10:24 +0530

    cors added

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
-const express = require("express");
-const { connectDB } = require("./config/database");
-const authRoute = require("./routes/authRoute");
-const { errorHandler } = require("./middlewares/error");
+const express = require('express');
+const cors = require('cors');
+const { connectDB } = require('./config/database');
+const authRoute = require('./routes/authRoute');
+const { errorHandler } = require('./middlewares/error');
 
 const app = express();
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+  })
+);
 connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/auth/v1", authRoute);
+app.use('/auth/v1', authRoute);
 app.use(errorHandler);
 
 module.exports = app;
