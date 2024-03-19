6b851715ca11fd85cda4139ffaf2f852fc49d0c2
MudassirQuraishi <mudassir.quraishi14@outlook.com>
2024-03-19 12:36:36 +0530

    Update error.js

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,15 +1,16 @@
-const { Logger } = require("../config/logger");
+const { Logger } = require('../config/logger');
+// eslint-disable-next-line no-unused-vars
 exports.errorHandler = (err, req, res, next) => {
-    Logger.log("error", {
-        req: req,
-        errorCode: err.name || "INTERNAL_SERVER_ERROR",
-        message: err.message || "Internal Server Error",
-        user: req.body || req.user || "User data not provided",
-    });
-    return res.status(err.statusCode).json({
-        error: {
-            errorCode: err.name || "INTERNAL_SERVER_ERROR",
-            message: err.message || "Please try again later",
-        },
-    });
+  Logger.log('error', {
+    req: req,
+    errorCode: err.name || 'INTERNAL_SERVER_ERROR',
+    message: err.message || 'Internal Server Error',
+    user: req.body || req.user || 'User data not provided',
+  });
+  return res.status(err.statusCode || 500).json({
+    error: {
+      errorCode: err.name || 'INTERNAL_SERVER_ERROR',
+      message: err.message || 'Please try again later',
+    },
+  });
 };
