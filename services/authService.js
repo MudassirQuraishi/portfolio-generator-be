ed0fd03172ab064873122d2c2e6578a552353d37
MudassirQuraishi <mudassir.quraishi14@outlook.com>
2024-03-19 12:35:32 +0530

    Updated authService.js

--- services/authService.js
+++ services/authService.js
@@ -1,19 +1,26 @@
-const ApiError = require("../util/ApiError");
+const ApiError = require('../util/ApiError');
 const {
-    PASSWORD_MISSMATCH,
-    EMAIL_NOT_VERIFIED,
-} = require("../util/errorMessages");
-const userService = require("./userService");
+  PASSWORD_MISSMATCH,
+  EMAIL_NOT_VERIFIED,
+  NOT_FOUND,
+} = require('../util/errorMessages');
+const userService = require('./userService');
 
 exports.loginUserWithEmailAndPassword = async (email, password) => {
-    const user = await userService.getUserByEmail(email);
-    if (!user || !user.isPasswordMatch(password)) {
-        const { code, message, name } = PASSWORD_MISSMATCH;
-        throw new ApiError(code, message, name);
-    }
-    if (!user.isEmailVerified) {
-        const { code, message, name } = EMAIL_NOT_VERIFIED;
-        throw new ApiError(code, message, name);
-    }
-    return user;
+  const user = await userService.getUserByEmail(email);
+  if (!user) {
+    const { code, message, name } = NOT_FOUND;
+    throw new ApiError(code, message, name);
+  }
+  const value = await user.isPasswordMatch(password);
+
+  if (!value) {
+    const { code, message, name } = PASSWORD_MISSMATCH;
+    throw new ApiError(code, message, name);
+  }
+  if (!user.isEmailVerified) {
+    const { code, message, name } = EMAIL_NOT_VERIFIED;
+    throw new ApiError(code, message, name);
+  }
+  return user;
 };
